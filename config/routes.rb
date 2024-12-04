48043bfdcef3b8936e66308bdd2b6948dc3f0fb8
ooyamadaizan <daizandeisia@gmail.com>
2024-12-04 15:59:21 +0900

    tasks/indexを作成
    gem 'simple_calendar'導入
    viewのファイル名が間違っていたので変更実施
    view.html.erb→index.html.erbに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  root :to: 'tasks/index'
+  root to: 'tasks#index'
   resources :tasks, only: [:new, :create]
 end
--- db/migrate/20241204033540_create_tasks.rb
+++ db/migrate/20241204033540_create_tasks.rb
@@ -1,7 +1,11 @@
 class CreateTasks < ActiveRecord::Migration[7.0]
   def change
     create_table :tasks do |t|
-
+      t.string     :title,       null: false
+      t.text       :description, null: false
+      t.date       :due_date,    null: false, index: true
+      t.boolean    :completed,   null: false, default: false
+      t.references :user,        null: false, foreign_key: true
       t.timestamps
     end
   end
