c61f85366d2db989433073e1fe5ce0305411c3c0
ooyamadaizan <daizandeisia@gmail.com>
2024-12-13 00:02:37 +0900

    タスク内のバックグラウンドをマークの色と同色にするよう設定
    色を薄くして見やすく設定

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,20 @@
 module TasksHelper
+  def color_to_rgb(color_name)
+    case color_name.downcase
+    when "red"
+      "255, 0, 0"
+    when "blue"
+      "0, 0, 255"
+    when "green"
+      "0, 255, 0"
+    when "yellow"
+      "255, 255, 0"
+    when "purple"
+      "128, 0, 128"
+    when "orange"
+      "255, 165, 0"
+    else
+      "0, 0, 0" # default to black if color is unknown
+    end
+  end
 end
